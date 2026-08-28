316e0e8f4e946e9f86a73813bf53dc659e389de5
mayurpawar0605 <mayurdpawar0605@gmail.com>
2026-08-29 00:10:38 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (69.14%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -5,17 +5,23 @@ class Solution {
         int j = s.length()-1;
 
         while(i >= 0){
-            //i wnat to find char not spaces
+            //we wnat to find char not spaces
             //if i get space -> -- both pointers
             if(s.charAt(i) == ' '){
                 i--;
                 j--;
             }else{
+                //j pointer on a char 
+                //i index will find space or go to less than 0
                 i--;
                 if(i < 0){
+                    //if i smaller than 0 we traverse on full string 
+                    //return current length
                     return j - i;
                 }
                 if(s.charAt(i) == ' '){
+                    //we again get empty space we find a word 
+                    //return length of that word
                     return j-i;
                 }
             }
