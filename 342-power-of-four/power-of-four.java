87bb49504595b3716f381c3e4f5987f01f5299ee
mayurpawar0605 <mayurdpawar0605@gmail.com>
2026-08-29 14:37:25 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (96.95%) - LeetSync

--- 342-power-of-four/power-of-four.java
+++ 342-power-of-four/power-of-four.java
@@ -11,23 +11,6 @@ class Solution {
         }
         return false;
 
-        // if(n == 1 || n == 4){
-        //     return true;
-        // }
-
-        // int s = 1;
-        // int e = n;
-
-        // while(s <= e){
-        //     int mid = s+(e-s)/2;
-        //     if(mid * 4 == n){
-        //         return true;
-        //     }else if (mid * 4 > n){
-        //         e = mid - 1;
-        //     }else{
-        //         s = mid + 1;
-        //     }
-        // }
-        // return false;
+        
     }
 }
