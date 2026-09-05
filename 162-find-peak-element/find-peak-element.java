4558dac4f378136f7b53c5c402b6a285b03344b6
mayurpawar0605 <mayurdpawar0605@gmail.com>
2026-09-05 12:50:02 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (87.99%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -2,27 +2,40 @@ class Solution {
     public int findPeakElement(int[] nums) {
         int n = nums.length;
 
-        if(n == 1){
-            return 0;
+        // if(n == 1){
+        //     return 0;
+        // }
+
+        // for(int  i = 0; i < n; i ++){
+        //     if(i > 0 && i < n-1){
+        //         if(nums[i] > nums[i-1] && nums[i] > nums[i+1]){
+        //             return i;
+        //         }
+        //     }
+        //     if(i == 0){
+        //         if(nums[0] > nums[1]){
+        //             return 0;
+        //         }
+        //     }
+        //     if(i == n-1){
+        //         if(nums[n-1] > nums[n-2]){
+        //             return n-1;
+        //         }
+        //     }
+        // }
+        // return -1;
+
+        int s = 0;
+        int e = n - 1;
+
+        while (s < e) {
+            int mid = s + (e  - s) / 2;
+            if(nums[mid] <= nums[mid + 1]) {
+                s = mid + 1;
+            }else{
+                e = mid;
+            }
         }
-        
-        for(int  i = 0; i < n; i ++){
-            if(i > 0 && i < n-1){
-                if(nums[i] > nums[i-1] && nums[i] > nums[i+1]){
-                    return i;
-                }
-            }
-            if(i == 0){
-                if(nums[0] > nums[1]){
-                    return 0;
-                }
-            }
-            if(i == n-1){
-                if(nums[n-1] > nums[n-2]){
-                    return n-1;
-                }
-            }
-        }
-        return -1;
+        return s;
     }
 }
