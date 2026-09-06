044d6d636f31ab8f98ee20484bdaea42bba0679a
mayurpawar0605 <mayurdpawar0605@gmail.com>
2026-09-06 17:45:50 +0530

    Time: 45 ms (46.75%) | Memory: 47.9 MB (26.22%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -2,21 +2,47 @@ class Solution {
    
     public int lengthOfLongestSubstring(String s) {
         
-        int n = s.length();
-        int maxLen = 0;
+        // int n = s.length();
+        // int maxLen = 0;
 
-        for(int i = 0; i < n ; i ++){
-            int[] arr = new int [256];
-            for(int j = i ; j < n; j ++){
-                char ch = s.charAt(j);
-                int index = ch;
-                if(arr[index] == 1){
-                    break;
-                }
-                int length = j - i + 1;
-                maxLen = Math.max(length,maxLen);
-                arr[index]++;
+        // for(int i = 0; i < n ; i ++){
+        //     int[] arr = new int [256];
+        //     for(int j = i ; j < n; j ++){
+        //         char ch = s.charAt(j);
+        //         int index = ch;
+        //         if(arr[index] == 1){
+        //             break;
+        //         }
+        //         int length = j - i + 1;
+        //         maxLen = Math.max(length,maxLen);
+        //         arr[index]++;
+        //     }
+        // }
+        // return maxLen;
+
+        int n = s.length();
+        HashMap<Character,Integer> map = new HashMap<>();
+        int maxLen = 0;
+        int l = 0;
+        int r = 0;
+        while (r < n){
+            char ch = s.charAt(r);
+            //if character repeats
+            if(map.containsKey(ch)){
+                //check for char repeats inside substring 
+                //repeated char index is greater than l -> update it
+                if(map.get(ch) >= l){
+                    //we need to update it 
+                    l = map.get(ch) + 1;
+                }   
             }
+            int length = r - l + 1;
+                maxLen = Math.max(length,maxLen);
+
+                //add current charater with current index in map
+                map.put(ch,r);
+                //check for nex character 
+                r++;
         }
         return maxLen;
     }
