59e6aebab3988d3a996035ecde0ed659ef3f8671
mayurpawar0605 <mayurdpawar0605@gmail.com>
2026-09-07 15:21:12 +0530

    Time: 3 ms (95.90%) | Memory: 52.4 MB (17.71%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -2,22 +2,47 @@ class Solution {
     public int longestOnes(int[] nums, int k) {
         int maxLength = 0;
         int n = nums.length;
+        int l = 0;
+        int r = 0;
+        int zeros = 0;
 
-        for(int i = 0; i < n; i++){
-            int noOfZeros = 0;
-            for(int j = i; j < n ; j ++){
-                if(nums[j] == 0){
-                    noOfZeros ++;
-                }
-                if(noOfZeros <= k){
-                    int length = j - i + 1;
-                    maxLength = Math.max(maxLength , length);
-                }else{
-                    //noOfZeros excedding k
-                    break;
-                }
+        //optinal approach
+
+        while(r < n){
+            if(nums[r] == 0){
+                zeros ++;
             }
+            if(zeros > k){
+                if(nums[l] == 0){
+                    zeros --;
+                }
+                l++;
+            }
+            if(zeros <= k){
+                //valid condition find length
+                int length = r - l + 1;
+                maxLength = Math.max(maxLength , length);
+            }
+            r++;
         }
+
+        //brute force
+
+        // for(int i = 0; i < n; i++){
+        //     int noOfZeros = 0;
+        //     for(int j = i; j < n ; j ++){
+        //         if(nums[j] == 0){
+        //             noOfZeros ++;
+        //         }
+        //         if(noOfZeros <= k){
+        //             int length = j - i + 1;
+        //             maxLength = Math.max(maxLength , length);
+        //         }else{
+        //             //noOfZeros excedding k
+        //             break;
+        //         }
+        //     }
+        // }
         return maxLength;
     }
 }
