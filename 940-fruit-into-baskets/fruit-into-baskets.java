125ce6786794ba914983feb33714bd4531c4dc0c
mayurpawar0605 <mayurdpawar0605@gmail.com>
2026-09-08 14:49:46 +0530

    Time: 53 ms (79.78%) | Memory: 68.2 MB (90.70%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -7,22 +7,38 @@ class Solution {
         HashMap<Integer,Integer> map = new HashMap<>();
 
         while(r < n){
-            //add freu of current element 
-            map.put(fruits[r], map.getOrDefault(fruits[r], 0) + 1);
+            map.put(fruits[r] ,map.getOrDefault(fruits[r],0) + 1);
 
-            //if map size exceeds 2
-            //map has 3 unique elements -> we have to shrink till map size less than two
-            // remove element at l till freq of that element is becomes 0
-            while(map.size() > 2){
-                map.replace(fruits[l], map.get(fruits[l]) - 1);
+            if(map.size() > 2){
+                map.replace(fruits[l],map.get(fruits[l]) - 1);
                 if(map.get(fruits[l]) == 0){
                     map.remove(fruits[l]);
                 }
                 l++;
             }
-            maxFruits = Math.max(maxFruits , r - l + 1);
+            if(map.size() <= 2){
+                maxFruits = Math.max(maxFruits , r - l + 1);
+            }
             r++;
         }
+
+        // while(r < n){
+        //     //add freu of current element 
+        //     map.put(fruits[r], map.getOrDefault(fruits[r], 0) + 1);
+
+        //     //if map size exceeds 2
+        //     //map has 3 unique elements -> we have to shrink till map size less than two
+        //     // remove element at l till freq of that element is becomes 0
+        //     while(map.size() > 2){
+        //         map.replace(fruits[l], map.get(fruits[l]) - 1);
+        //         if(map.get(fruits[l]) == 0){
+        //             map.remove(fruits[l]);
+        //         }
+        //         l++;
+        //     }
+        //     maxFruits = Math.max(maxFruits , r - l + 1);
+        //     r++;
+        // }
 
     //    for(int i = 0; i < n; i ++){
     //         HashSet<Integer> set = new HashSet<>();
