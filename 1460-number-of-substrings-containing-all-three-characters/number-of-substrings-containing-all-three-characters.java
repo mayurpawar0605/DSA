69162cc79081614fccc7af37f6b3ba7848811667
mayurpawar0605 <mayurdpawar0605@gmail.com>
2026-09-09 17:25:43 +0530

    Time: 11 ms (89.13%) | Memory: 46.3 MB (47.53%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -21,16 +21,15 @@ class Solution {
         // }
 
         int n = s.length();
-        int i =0;
         int count = 0;
-        int lastSeen[] = {-1,-1,-1};
+        int lastSeen[] = { -1, -1, -1 };
 
-        while(i < n){
+        for (int i = 0; i < n; i++) {
             lastSeen[s.charAt(i) - 'a'] = i;
-            if(lastSeen[0] != -1 && lastSeen[1] != -1 && lastSeen[2] != -1){
-                count = count + 1 + Math.min(lastSeen[0], Math.min(lastSeen[1], lastSeen[2]));
-            }
-            i++;
+            // if(lastSeen[0] != -1 && lastSeen[1] != -1 && lastSeen[2] != -1){
+            //     count = count + 1 + Math.min(lastSeen[0], Math.min(lastSeen[1], lastSeen[2]));
+            // }
+            count = count + 1 + Math.min(lastSeen[0], Math.min(lastSeen[1], lastSeen[2]));
         }
         return count;
     }
