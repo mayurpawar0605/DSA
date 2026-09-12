e0c861c192019b6e1a56803472961e9618a8df5f
mayurpawar0605 <mayurdpawar0605@gmail.com>
2026-09-12 23:08:12 +0530

    Time: 12 ms (44.34%) | Memory: 46.3 MB (38.20%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -21,6 +21,36 @@ class Solution {
         // }
         // return maxLen;
 
+        // int l = 0;
+        // int r = 0;
+        // int maxLen = 0;
+        // int maxF = 0;
+        // int[] arr = new int [26];
+        // int n = s.length();
+
+        // while(r < n){
+        //     //increase freq of current char by 1
+        //     arr[s.charAt(r) - 'A'] ++;
+        //     maxF = Math.max(maxF, arr[s.charAt(r) -'A']);
+
+        //     while((r - l + 1) - maxF > k){
+        //         //shrink -> maxF exceeds k
+        //         arr[s.charAt(l) - 'A'] --;
+        //         for(int i = 0; i < 26;i ++){
+        //             maxF = Math.max(maxF,arr[i]);
+        //         }
+        //         l++;
+        //     }
+        //     //valid case
+        //     if((r - l + 1) - maxF <= k){
+        //         maxLen = Math.max(maxLen, r - l + 1);
+        //     }
+        //     r++;
+
+        // }
+        // return maxLen;
+
+
         int l = 0;
         int r = 0;
         int maxLen = 0;
@@ -33,12 +63,9 @@ class Solution {
             arr[s.charAt(r) - 'A'] ++;
             maxF = Math.max(maxF, arr[s.charAt(r) -'A']);
 
-            while((r - l + 1) - maxF > k){
+            if((r - l + 1) - maxF > k){
                 //shrink -> maxF exceeds k
                 arr[s.charAt(l) - 'A'] --;
-                for(int i = 0; i < 26;i ++){
-                    maxF = Math.max(maxF,arr[i]);
-                }
                 l++;
             }
             //valid case
