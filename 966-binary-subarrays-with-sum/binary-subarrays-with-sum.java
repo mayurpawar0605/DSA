aa2b1e880939881dc637494db1a7de1e50f6311b
mayurpawar0605 <mayurdpawar0605@gmail.com>
2026-09-14 00:35:36 +0530

    Time: 2 ms (62.17%) | Memory: 51.2 MB (30.12%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,10 +1,12 @@
 class Solution {
     static int numberOfSubArraysWithSumLessThanEqualToGoal(int[] nums,int goal){
-
+        //if goal = 0;
+        //then gaol -1 = -1
+        //ans = 0 - (-1) = 0  + 1 wrong 
         if(goal < 0){
             return 0;
         }
-        
+
         int r = 0;
         int l = 0;
         int sum = 0;
@@ -26,6 +28,10 @@ class Solution {
 
     }
     public int numSubarraysWithSum(int[] nums, int goal) {
-        return numberOfSubArraysWithSumLessThanEqualToGoal(nums,goal) - numberOfSubArraysWithSumLessThanEqualToGoal(nums,goal - 1);
+        //it can be solved by prefix sum 
+        //but this is most optonam approach 
+        //sum == goal  =  sum <= gaol  - sum <= gaol - 1;
+        return 
+        numberOfSubArraysWithSumLessThanEqualToGoal(nums,goal) - numberOfSubArraysWithSumLessThanEqualToGoal(nums,goal - 1);
     }   
 }
