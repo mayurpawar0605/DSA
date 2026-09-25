1403f9129370a65e4435b0920be9603434247090
mayurpawar0605 <mayurdpawar0605@gmail.com>
2026-09-26 00:31:54 +0530

    Time: 19 ms (81.23%) | Memory: 45.9 MB (31.94%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -26,25 +26,39 @@ class Solution {
 
         */
 
-        /*
+        Arrays.sort(arr);
         int n = arr.length;
-        Set<List<Integer>> ans = new HashSet<>();
+        List<List<Integer>> ans = new ArrayList<>();
 
         for(int i = 0; i < n;i++){
+            if(i > 0 && arr[i-1] == arr[i]) continue;
+
             for(int j = i + 1; j < n;j++){
+                if(j > i+1 && arr[j-1] == arr[j]) continue;
+
                 int k = j + 1;
                 int l = n -1;
                 while(k < l){
-                    int sum = arr[i] + arr[j] + arr[k] + arr[l];
+                    long sum = arr[i] + arr[j];
+                    sum += arr[k] + arr[l];
+
                     if(sum == target){
                         List<Integer> temp = new ArrayList<>();
                         temp.add(arr[i]);
                         temp.add(arr[j]);
                         temp.add(arr[k]);
                         temp.add(arr[l]);
-                        Collections.sort(temp);
                         ans.add(temp);
+
                         k++;
+                        l--;
+                        //we need unique elements
+                        while(k < n && arr[k] == arr[k-1]){
+                            k++;
+                        }while(l > k && arr[l] == arr[l+1]){
+                            l--;
+                        }
+
                     }else if (sum > target){
                         l--;
                     }else{
@@ -53,9 +67,10 @@ class Solution {
                 }
             }
         }
-        return new ArrayList<>(ans);
-        */
+        return ans;
+        
 
+        /*
         int n = arr.length;
         Set<List<Integer>> ans = new HashSet<>();
 
@@ -80,5 +95,6 @@ class Solution {
             }
         }
         return new ArrayList<>(ans);
+        */
     }
 }
